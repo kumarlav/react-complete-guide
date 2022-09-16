bdf49ed0b4738108cc78933b7720a04531f6d313
kumarlav007 <lav.kumar@royalcyber.com>
2022-09-16 16:20:47 +0530

    react app from scratch

---
+++ src/compnents/Expenses/Expenses.js
@@ -0,0 +1,38 @@
+import React, { useState } from 'react'
+import ExpenseItem from './ExpenseItem';
+import './Expenses.css'
+import Card from '../UI/Card'
+import ExpensesFilter from './ExpensesFilter'
+
+const Expenses = (props) => {
+
+  const [filteredYear, setFilteredYear] = useState('2020');
+
+  const filterChangeHandler = (selectedYear) => {
+    setFilteredYear(selectedYear);
+  };
+
+  return (
+    <Card className='expenses'>
+      <ExpensesFilter selected={filteredYear} onChangeFilter={filterChangeHandler} />
+      {
+        props.items
+          .filter(expense => {
+            if(expense.date.getFullYear().toString() === filteredYear)
+              return true;
+            return false;
+          })
+          .map(expense => {
+            return <ExpenseItem key={expense.id}
+              title={expense.title}
+              amount={expense.amount}
+              date={expense.date}
+            />
+          })
+      }
+
+    </Card>
+  )
+}
+
+export default Expenses
